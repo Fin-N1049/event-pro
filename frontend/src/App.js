2e7d0c395cd80205668f84975f39225a071cdac8
Shiyasalloor <shiyas6082@gmail.com>
2024-10-20 04:02:04 +0530

    team listing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TeamListing from './screens/team_listing/team_listing.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header >
+        <TeamListing />
       </header>
     </div>
   );
